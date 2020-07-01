71e014e6c757406a34532222d6531563c386b1f2
Tharusha Perera <tharushaperera2018@gmail.com>
2020-07-01 18:21:09 +0530

    to do app added

---
+++ to do app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>hello project</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
